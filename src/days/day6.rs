273b8e88e3eb9202fe98de98bc3b8c550c0de2c0
Thijs Waalen <contact@thijswaalen.com>
2022-12-29 15:40:35 +0100

    Day 6: Make marker size configurable and rename function

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,10 +1,10 @@
-use std::io::{Read, BufReader};
+use std::{io::{Read, BufReader}, usize::MAX};
 
-pub fn find_first_start_of_packet_marker_index(data: &str) -> i32 {
+pub fn find_end_of_marker_index(data: &str, marker_size: usize) -> usize {
     for i in 0..data.len() {
-        let check_slice = &data[i..i + 4];
+        let check_slice = &data[i..i + marker_size];
         let mut found_start_of_packet_marker = true;
-        for j in 0..4 {
+        for j in 0..marker_size {
             let last_index_of_char = check_slice.rfind(check_slice.chars().nth(j).unwrap()).unwrap();
             if last_index_of_char != j {
                 found_start_of_packet_marker = false;
@@ -13,16 +13,16 @@ pub fn find_first_start_of_packet_marker_index(data: &str) -> i32 {
         }
 
         if found_start_of_packet_marker {
-            return i as i32 + 4;
+            return i + marker_size;
         }
     }
 
-    return -1;
+    return MAX;
 }
 
 pub fn solve<R>(input: &mut BufReader<R>) -> String where R : Read {
     let mut data = Default::default();
     input.read_to_string(&mut data).expect("Failed to read input");
 
-    return find_first_start_of_packet_marker_index(&data).to_string();
+    return find_end_of_marker_index(&data, 4).to_string();
 }
--- tests/day6.rs
+++ tests/day6.rs
@@ -1,4 +1,4 @@
-use aoc2022::days::day6::find_first_start_of_packet_marker_index;
+use aoc2022::days::day6::find_end_of_marker_index;
 
 const TEST_INPUT: [&str; 5] = [
     "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
@@ -10,9 +10,9 @@ const TEST_INPUT: [&str; 5] = [
 
 #[test]
 fn correct_start_of_packet_marker_index() {
-    assert_eq!(7, find_first_start_of_packet_marker_index(TEST_INPUT[0]));
-    assert_eq!(5, find_first_start_of_packet_marker_index(TEST_INPUT[1]));
-    assert_eq!(6, find_first_start_of_packet_marker_index(TEST_INPUT[2]));
-    assert_eq!(10, find_first_start_of_packet_marker_index(TEST_INPUT[3]));
-    assert_eq!(11, find_first_start_of_packet_marker_index(TEST_INPUT[4]));
+    assert_eq!(7, find_end_of_marker_index(TEST_INPUT[0], 4));
+    assert_eq!(5, find_end_of_marker_index(TEST_INPUT[1], 4));
+    assert_eq!(6, find_end_of_marker_index(TEST_INPUT[2], 4));
+    assert_eq!(10, find_end_of_marker_index(TEST_INPUT[3], 4));
+    assert_eq!(11, find_end_of_marker_index(TEST_INPUT[4], 4));
 }
